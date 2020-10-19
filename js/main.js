08eb8a344e998edea74079df03da78b74b920f1a
Nathan Cohen <ncohen033@gmail.com>
2020-10-19 09:57:26 -0700

    wheresaaron

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-// $(document).ready(function(){
+ $(document).ready(function(){
+
+
+    //NAVBAR TOGGLE
+    let button = document.querySelector('.collapse-nav');
+    let dropDown = document.querySelector('.dropdown');
+
+    button.addEventListener('click', function(event){
+        dropDown.classList.toggle('show');
+    })
   
-
- 
-
-// });
+ });
